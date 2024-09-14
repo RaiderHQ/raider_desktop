662d77e49e0be6d9f4747dd727c29e4724c8050f
Bogdan Hossu <hossubogdan@gmail.com>
2024-09-15 01:20:58 +0200

    Add question mark tooltip

--- src/renderer/src/components/ProjectSelector/index.tsx
+++ src/renderer/src/components/ProjectSelector/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import QuestionIcon from '../../assets/icons/Question_vector.svg'
 
 interface ProjectSelectorProps {
   icon: string
@@ -18,6 +19,9 @@ const ProjectSelector: React.FC<ProjectSelectorProps> = ({
     <div className="relative flex flex-col items-center justify-center">
       <div className="absolute -right-1 -bottom-1 w-full h-full bg-[#c14420] rounded-lg" />
       <div className="relative flex flex-col items-center justify-center border border-black p-8 rounded-lg bg-white z-10">
+        <div className="absolute top-2 right-2">
+          <img src={QuestionIcon} className="w-4 h-auto" />
+        </div>
         <div className="mb-4">
           <img src={icon} className="w-12 h-auto" />
         </div>
