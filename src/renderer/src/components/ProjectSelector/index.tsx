49ebfd0c5217bdfcd4488f7c2092bea77f4f4bce
Bogdan Hossu <hossubogdan@gmail.com>
2024-09-15 01:17:08 +0200

    Add shadow for boxes

--- src/renderer/src/components/ProjectSelector/index.tsx
+++ src/renderer/src/components/ProjectSelector/index.tsx
@@ -15,14 +15,17 @@ const ProjectSelector: React.FC<ProjectSelectorProps> = ({
   buttonValue
 }): JSX.Element => {
   return (
-    <div className="flex flex-col items-center justify-center border-2 border-gray-300 p-8 rounded-lg shadow-lg">
-      <div className="mb-4">
-        <img src={icon} className="w-12 h-auto" />
+    <div className="relative flex flex-col items-center justify-center">
+      <div className="absolute -right-1 -bottom-1 w-full h-full bg-[#c14420] rounded-lg" />
+      <div className="relative flex flex-col items-center justify-center border border-black p-8 rounded-lg bg-white z-10">
+        <div className="mb-4">
+          <img src={icon} className="w-12 h-auto" />
+        </div>
+        <h2 className="text-xl font-semibold text-gray-900 mb-6">{description}</h2>
+        <Link to={url} className="px-4 py-2 bg-gray-300 text-blue-500 rounded-lg shadow">
+          {buttonValue}
+        </Link>
       </div>
-      <h2 className="text-xl font-semibold text-gray-900 mb-6">{description}</h2>
-      <Link to={url} className="px-4 py-2 bg-gray-300 text-blue-500 rounded-lg shadow">
-        {buttonValue}
-      </Link>
     </div>
   )
 }
