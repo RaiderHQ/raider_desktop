e3f34f6ee826216cc496ea70a6a99ed2b8e4d61c
Bogdan Hossu <hossubogdan@gmail.com>
2024-11-03 15:30:28 +0100

    Hide menu on create page

--- src/renderer/src/templates/Main/index.tsx
+++ src/renderer/src/templates/Main/index.tsx
@@ -1,9 +1,10 @@
-import { Outlet, Link } from 'react-router-dom'
+import { Outlet, Link, useLocation } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
 import Logo from '@assets/images/logo-with-title.svg'
 
 const MainTemplate: React.FC = (): JSX.Element => {
   const { t } = useTranslation()
+  const location = useLocation()
   const raiderVersion = import.meta.env.VITE_RAIDER_VERSION
 
   return (
@@ -12,14 +13,16 @@ const MainTemplate: React.FC = (): JSX.Element => {
         <div className="flex items-center space-x-4">
           <img src={Logo} alt="Logo" className="w-100 h-10" />
         </div>
-        <nav className="flex space-x-8">
-          <Link to="/project/overview" className="text-gray-600 hover:text-gray-800">
-            {t('menu.tests')}
-          </Link>
-          <Link to="/project/settings" className="text-gray-600 hover:text-gray-800">
-            {t('menu.settings')}
-          </Link>
-        </nav>
+        {location.pathname === '/project/overview' && (
+          <nav className="flex space-x-8">
+            <Link to="/project/overview" className="text-gray-600 hover:text-gray-800">
+              {t('menu.tests')}
+            </Link>
+            <Link to="/project/settings" className="text-gray-600 hover:text-gray-800">
+              {t('menu.settings')}
+            </Link>
+          </nav>
+        )}
       </header>
 
       <main className="flex flex-col items-center justify-center flex-grow">
