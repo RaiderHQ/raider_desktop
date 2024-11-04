07a5e32c2078e47ff9a14619e06ac0025918043e
Bogdan Hossu <hossubogdan@gmail.com>
2024-11-04 22:43:12 +0100

    Check if config exist when selecting folder

--- src/main/handlers/selectFolder.ts
+++ src/main/handlers/selectFolder.ts
@@ -1,4 +1,7 @@
 import { dialog, IpcMainInvokeEvent } from 'electron'
+import fs from 'fs'
+import path from 'path'
+import { SETTINGS_FILE } from '../../renderer/src/foundation/Constants'
 
 const selectFolder = async (_event: IpcMainInvokeEvent, title: string): Promise<string | null> => {
   const result = await dialog.showOpenDialog({
@@ -6,7 +9,19 @@ const selectFolder = async (_event: IpcMainInvokeEvent, title: string): Promise<
     title
   })
 
-  return result.canceled ? null : result.filePaths[0]
+  if (result.canceled) {
+    return null
+  }
+
+  const folderPath = result.filePaths[0]
+  const settingsFilePath = path.join(folderPath, SETTINGS_FILE)
+  try {
+    await fs.promises.access(settingsFilePath)
+  } catch {
+    throw new Error(`Settings file not found at ${settingsFilePath}`)
+  }
+
+  return folderPath
 }
 
 export default selectFolder
