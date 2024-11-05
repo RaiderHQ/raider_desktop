d4244a4b4b036c77b6706beb503471886d1ded9c
Bogdan Hossu <hossubogdan@gmail.com>
2024-11-05 15:58:22 +0100

    Change logic for project config check / Check project config separately

--- src/renderer/src/foundation/Types/window.ts
+++ src/renderer/src/foundation/Types/window.ts
@@ -6,7 +6,11 @@ export interface WindowApi {
     createSettingsFile: (
       folder: string,
       data: Record<string, unknown>
-    ) => Promise<{ success: boolean; filePath?: string; error?: string }>,
+    ) => Promise<{ success: boolean; filePath?: string; error?: string }>
     readDirectory: (folder: string) => Promise<FileNode[]>
+    readFile: (file: string) => Promise<string>
+    checkConfig: (
+      folderPath: string
+    ) => Promise<{ success: boolean; filePath?: string; error?: string }>
   }
 }
