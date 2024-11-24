a031c2995010a0b9b9ba134ba91ac13e5d808cbb
aguspe <agustin.pe94@gmail.com>
2024-11-24 08:16:43 +0100

    Remove checkbox next to files

--- src/renderer/src/components/Library/File/index.tsx
+++ src/renderer/src/components/Library/File/index.tsx
@@ -6,26 +6,12 @@ import { FileNode } from '@foundation/Types/fileNode'
 
 const File: React.FC<FileNode> = ({ name, path }) => {
   const navigate = useNavigate()
-  const toggleFile: (path: string) => void = useProjectStore(
-    (state: { toggleFile: (path: string) => void }) => state.toggleFile
-  )
-  const selectedFiles: string[] = useProjectStore(
-    (state: { selectedFiles: string[] }) => state.selectedFiles
-  )
-  const isSelected = selectedFiles.includes(path)
 
   return (
     <div
       className="flex items-center px-4 py-2 border-b cursor-pointer hover:bg-gray-100 hover:text-blue-600"
       onClick={() => navigate('/project/editor', { state: { fileName: name, path } })}
     >
-      <input
-        type="checkbox"
-        className="mr-2 w-4 h-4 cursor-pointer"
-        checked={isSelected}
-        onChange={() => toggleFile(path)}
-        onClick={(e) => e.stopPropagation()}
-      />
       <FaFileAlt className="mr-2 text-gray-600" />
       <span>{name}</span>
     </div>
