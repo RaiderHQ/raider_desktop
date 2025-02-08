c0623a000359987a875a6173603e7a1bc7a82ba7
aguspe <agustin.pe94@gmail.com>
2025-02-08 16:02:35 +0100

    Add new alert notification

---
+++ src/renderer/src/components/Alert/index.tsx
@@ -0,0 +1,41 @@
+import ReactDOM from 'react-dom'
+
+interface AlertProps {
+  message: string
+  onClose: () => void
+}
+
+const Alert: React.FC<AlertProps> = ({ message, onClose }) => {
+  const handleOutsideClick = (e: React.MouseEvent) => {
+    if ((e.target as HTMLElement).id === 'alert-modal-overlay') {
+      onClose()
+    }
+  }
+
+  return ReactDOM.createPortal(
+    <div
+      id="alert-modal-overlay"
+      className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50"
+      onClick={handleOutsideClick}
+    >
+      <div
+        className="bg-white rounded-lg shadow-lg max-w-lg w-full p-6 relative border-4 border-[#c14420]"
+        onClick={(e) => e.stopPropagation()}
+      >
+        <div className="mb-4">
+          <p className="text-black">{message}</p>
+        </div>
+        <button
+          className="absolute top-1 right-3 text-black hover:text-black text-3xl"
+          onClick={onClose}
+          aria-label="Close"
+        >
+          ×
+        </button>
+      </div>
+    </div>,
+    document.getElementById('root')!
+  )
+}
+
+export default Alert
