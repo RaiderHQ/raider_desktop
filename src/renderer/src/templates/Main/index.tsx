2b196963c7c8f07888a932dc41ab26f5ec7f753f
aguspe <agustin.pe94@gmail.com>
2025-06-15 18:31:02 +0200

    button looking good

--- src/renderer/src/templates/Main/index.tsx
+++ src/renderer/src/templates/Main/index.tsx
@@ -15,21 +15,21 @@ const MainTemplate: React.FC = (): JSX.Element => {
     <div className="min-h-screen bg-white flex flex-col">
       <header className="flex items-center justify-between bg-white px-8 py-4 border-b border-black">
         <div className="flex items-center space-x-4">
-          <img src={Logo} alt="Logo" className="w-100 h-10" />
+          <img src={Logo} alt="Logo" className="w-100 h-13" />
         </div>
 
         {!isCreateProjectView && (
           <nav className="flex space-x-8">
-            <Link to="/project/overview" className="text-gray-600 hover:text-gray-800">
+            <Link to="/project/overview" className="text-gray-600 hover:text-gray-800 text-lg">
               {t('menu.tests')}
       </Link>
-      <Link to="/project/recorder" className="text-gray-600 hover:text-gray-800">
+      <Link to="/project/recorder" className="text-gray-600 hover:text-gray-800 text-lg">
         Recorder
             </Link>
-            <Link to="/project/dashboard" className="text-gray-600 hover:text-gray-800">
+            <Link to="/project/dashboard" className="text-gray-600 hover:text-gray-800 text-lg">
               {t('menu.dashboard')}
             </Link>
-            <Link to="/project/settings" className="text-gray-600 hover:text-gray-800">
+            <Link to="/project/settings" className="text-gray-600 hover:text-gray-800 text-lg">
               {t('menu.settings')}
             </Link>
           </nav>
