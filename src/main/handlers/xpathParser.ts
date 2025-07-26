ed4de49e740028a68a2770873835e63053e55d30
aguspe <agustin.pe94@gmail.com>
2025-07-26 02:03:18 +0200

    xpath working

--- src/main/handlers/xpathParser.ts
+++ src/main/handlers/xpathParser.ts
@@ -4,94 +4,168 @@ export interface ParsedCommand {
 }
 
 const xpathParser = (command: string): ParsedCommand | string => {
-  console.log('[xpathParser.ts] Executing with command:', command)
-  // This regex part captures any of the possible selector quoting styles for XPath
-  const selectorRegexPart = `(?:concat\((.+?)\)|"((?:\\"|[^"])*)"|'((?:\\'|[^'])*)')`
-
   const patterns: {
     regex: RegExp
     template: (matches: string[]) => ParsedCommand
   }[] = [
+    // waitForElementDisplayed
     {
-      regex: new RegExp(
-        `wait\\.until { @driver\\.find_element\\(:xpath,\\s*${selectorRegexPart}\\)\\.displayed\\? }`
-      ),
+      regex: /wait\.until { @driver\.find_element\(:xpath,\s*'([^']*)'\)\.displayed\? }$/,
       template: (matches: string[]): ParsedCommand => ({
         key: 'recorder.commandParser.waitForElementDisplayed',
-        values: { strategy: 'xpath', value: matches[1] || matches[2] || matches[3] }
+        values: { strategy: 'xpath', value: matches[1] }
       })
     },
     {
-      regex: new RegExp(
-        `wait\\.until { @driver\\.find_element\\(:xpath,\\s*${selectorRegexPart}\\)\\.enabled\\? }`
-      ),
+      regex: /wait\.until { @driver\.find_element\(:xpath,\s*"([^"]*)"\)\.displayed\? }$/,
+      template: (matches: string[]): ParsedCommand => ({
+        key: 'recorder.commandParser.waitForElementDisplayed',
+        values: { strategy: 'xpath', value: matches[1] }
+      })
+    },
+    {
+      regex: /wait\.until { @driver\.find_element\(:xpath,\s*concat\((.+?)\)\)\.displayed\? }$/,
+      template: (matches: string[]): ParsedCommand => ({
+        key: 'recorder.commandParser.waitForElementDisplayed',
+        values: { strategy: 'xpath', value: matches[1] }
+      })
+    },
+
+    // waitForElementEnabled
+    {
+      regex: /wait\.until { @driver\.find_element\(:xpath,\s*'([^']*)'\)\.enabled\? }/,
       template: (matches: string[]): ParsedCommand => ({
         key: 'recorder.commandParser.waitForElementEnabled',
-        values: { strategy: 'xpath', value: matches[1] || matches[2] || matches[3] }
+        values: { strategy: 'xpath', value: matches[1] }
       })
     },
     {
-      regex: new RegExp(`@driver\\.find_element\\(:xpath,\\s*${selectorRegexPart}\\)\\.click`),
+      regex: /wait\.until { @driver\.find_element\(:xpath,\s*"([^"]*)"\)\.enabled\? }/,
+      template: (matches: string[]): ParsedCommand => ({
+        key: 'recorder.commandParser.waitForElementEnabled',
+        values: { strategy: 'xpath', value: matches[1] }
+      })
+    },
+    {
+      regex: /wait\.until { @driver\.find_element\(:xpath,\s*concat\((.+?)\)\)\.enabled\? }/,
+      template: (matches: string[]): ParsedCommand => ({
+        key: 'recorder.commandParser.waitForElementEnabled',
+        values: { strategy: 'xpath', value: matches[1] }
+      })
+    },
+
+    // clickElement
+    {
+      regex: /@driver\.find_element\(:xpath,\s*'([^']*)'\)\.click/,
       template: (matches: string[]): ParsedCommand => ({
         key: 'recorder.commandParser.clickElement',
-        values: { strategy: 'xpath', value: matches[1] || matches[2] || matches[3] }
+        values: { strategy: 'xpath', value: matches[1] }
       })
     },
     {
-      regex: new RegExp(
-        `@driver\\.find_element\\(:xpath,\\s*${selectorRegexPart}\\)\\.send_keys\\(:(\\w+)\\)`
-      ),
+      regex: /@driver\.find_element\(:xpath,\s*"([^"]*)"\)\.click/,
       template: (matches: string[]): ParsedCommand => ({
-        key: 'recorder.commandParser.pressKey',
-        values: {
-          key: matches[4],
-          strategy: 'xpath',
-          value: matches[1] || matches[2] || matches[3]
-        }
+        key: 'recorder.commandParser.clickElement',
+        values: { strategy: 'xpath', value: matches[1] }
       })
     },
     {
-      regex: new RegExp(
-        `@driver\\.find_element\\(:xpath,\\s*${selectorRegexPart}\\)\\.send_keys\\("([^"]*)"\\)`
-      ),
+      regex: /@driver\.find_element\(:xpath,\s*concat\((.+?)\)\)\.click/,
+      template: (matches: string[]): ParsedCommand => ({
+        key: 'recorder.commandParser.clickElement',
+        values: { strategy: 'xpath', value: matches[1] }
+      })
+    },
+
+    // typeText
+    {
+      regex: /@driver\.find_element\(:xpath,\s*'([^']*)'\)\.send_keys\("([^"]*)"\)/,
       template: (matches: string[]): ParsedCommand => ({
         key: 'recorder.commandParser.typeText',
-        values: {
-          text: matches[4],
-          strategy: 'xpath',
-          value: matches[1] || matches[2] || matches[3]
-        }
+        values: { strategy: 'xpath', value: matches[1], text: matches[2] }
       })
     },
     {
-      regex: new RegExp(
-        `expect\\(@driver\\.find_element\\(:xpath,\\s*${selectorRegexPart}\\)\\)\\.to be_displayed$`
-      ),
+      regex: /@driver\.find_element\(:xpath,\s*"([^"]*)"\)\.send_keys\("([^"]*)"\)/,
+      template: (matches: string[]): ParsedCommand => ({
+        key: 'recorder.commandParser.typeText',
+        values: { strategy: 'xpath', value: matches[1], text: matches[2] }
+      })
+    },
+    {
+      regex: /@driver\.find_element\(:xpath,\s*concat\((.+?)\)\)\.send_keys\("([^"]*)"\)/,
+      template: (matches: string[]): ParsedCommand => ({
+        key: 'recorder.commandParser.typeText',
+        values: { strategy: 'xpath', value: matches[1], text: matches[2] }
+      })
+    },
+
+    // assertElementDisplayed
+    {
+      regex: /expect\(@driver\.find_element\(:xpath,\s*'([^']*)'\)\)\.to be_displayed$/,
       template: (matches: string[]): ParsedCommand => ({
         key: 'recorder.commandParser.assertElementDisplayed',
-        values: { strategy: 'xpath', value: matches[1] || matches[2] || matches[3] }
+        values: { strategy: 'xpath', value: matches[1] }
       })
     },
     {
-      regex: new RegExp(
-        `expect\\(@driver\\.find_element\\(:xpath,\\s*${selectorRegexPart}\\)\\)\\.to be_enabled`
-      ),
+      regex: /expect\(@driver\.find_element\(:xpath,\s*"([^"]*)"\)\)\.to be_displayed$/,
+      template: (matches: string[]): ParsedCommand => ({
+        key: 'recorder.commandParser.assertElementDisplayed',
+        values: { strategy: 'xpath', value: matches[1] }
+      })
+    },
+    {
+      regex: /expect\(@driver\.find_element\(:xpath,\s*concat\((.+?)\)\)\)\.to be_displayed$/,
+      template: (matches: string[]): ParsedCommand => ({
+        key: 'recorder.commandParser.assertElementDisplayed',
+        values: { strategy: 'xpath', value: matches[1] }
+      })
+    },
+
+    // assertElementEnabled
+    {
+      regex: /expect\(@driver\.find_element\(:xpath,\s*'([^']*)'\)\)\.to be_enabled/,
       template: (matches: string[]): ParsedCommand => ({
         key: 'recorder.commandParser.assertElementEnabled',
-        values: { strategy: 'xpath', value: matches[1] || matches[2] || matches[3] }
+        values: { strategy: 'xpath', value: matches[1] }
       })
     },
     {
-      regex: new RegExp(
-        `expect\\(@driver\\.find_element\\(:xpath,\\s*${selectorRegexPart}\\)\\.text\\)\\.to eq\\("([^"]+)"\\)`
-      ),
+      regex: /expect\(@driver\.find_element\(:xpath,\s*"([^"]*)"\)\)\.to be_enabled/,
+      template: (matches: string[]): ParsedCommand => ({
+        key: 'recorder.commandParser.assertElementEnabled',
+        values: { strategy: 'xpath', value: matches[1] }
+      })
+    },
+    {
+      regex: /expect\(@driver\.find_element\(:xpath,\s*concat\((.+?)\)\)\)\.to be_enabled/,
+      template: (matches: string[]): ParsedCommand => ({
+        key: 'recorder.commandParser.assertElementEnabled',
+        values: { strategy: 'xpath', value: matches[1] }
+      })
+    },
+
+    // assertElementTextEquals
+    {
+      regex: /expect\(@driver\.find_element\(:xpath,\s*'([^']*)'\)\.text\)\.to eq\("([^"]+)"\)/,
       template: (matches: string[]): ParsedCommand => ({
         key: 'recorder.commandParser.assertElementTextEquals',
-        values: {
-          strategy: 'xpath',
-          value: matches[1] || matches[2] || matches[3],
-          text: matches[4]
-        }
+        values: { strategy: 'xpath', value: matches[1], text: matches[2] }
+      })
+    },
+    {
+      regex: /expect\(@driver\.find_element\(:xpath,\s*"([^"]*)"\)\.text\)\.to eq\("([^"]+)"\)/,
+      template: (matches: string[]): ParsedCommand => ({
+        key: 'recorder.commandParser.assertElementTextEquals',
+        values: { strategy: 'xpath', value: matches[1], text: matches[2] }
+      })
+    },
+    {
+      regex: /expect\(@driver\.find_element\(:xpath,\s*concat\((.+?)\)\)\.text\)\.to eq\("([^"]+)"\)/,
+      template: (matches: string[]): ParsedCommand => ({
+        key: 'recorder.commandParser.assertElementTextEquals',
+        values: { strategy: 'xpath', value: matches[1], text: matches[2] }
       })
     }
   ]
@@ -99,12 +173,10 @@ const xpathParser = (command: string): ParsedCommand | string => {
   for (const pattern of patterns) {
     const match = command.match(pattern.regex)
     if (match) {
-      console.log('[xpathParser.ts] Found a match!', match)
       return pattern.template(match)
     }
   }
 
-  console.log('[xpathParser.ts] No match found, returning original command.')
   return command
 }
 
